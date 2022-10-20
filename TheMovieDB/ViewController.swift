49f1b567a7714565eddd296d5756bd9f89eda75a
Mostafa Nafie <MostafaTarekNafie@gmail.com>
2022-10-20 07:47:55 +0200

    Initial commit

---
+++ TheMovieDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMovieDB
+//
+//  Created by Mostafa Nafie on 20/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
